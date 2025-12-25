4ad6434e0f537f23661dd61b06c31ca213e73c31
Cheesecake2960 <155858568+Cheesecake2960@users.noreply.github.com>
2025-12-25 09:45:41 +0900

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Home() {
+  const [childWindow, setChildWindow] = useState<Window | null>(null);
+  
+  const windowHeight = 400;
+  const windowWidth = 400;
+
+  function moveChildWindow(x: number, y: number) {
+    childWindow?.moveBy(x, y);
+  }
+
+  const onClickOpenButton = () => {
+    setChildWindow(window.open("about:blank", "_blank", `width=${windowWidth},height=${windowHeight},resizable=no`));
+    setTimeout(moveChildWindow, 1000);
+  }
+
+  return (
+    <div>
+      <button onClick={onClickOpenButton}>Open</button>
+      <div>
+        <button onClick={() => moveChildWindow(0, -100)}>🔼</button>
+        <button onClick={() => moveChildWindow(0, 100)}>🔽</button>
+        <button onClick={() => moveChildWindow(-100, 0)}>◀️</button>
+        <button onClick={() => moveChildWindow(100, 0)}>▶️</button>
+      </div>
+    </div>
+  );
+}
